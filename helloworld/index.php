444f1692f98c15a3235adfd2e9a281587866f891
jbraunstein-do <94200286+jbraunstein-do@users.noreply.github.com>
2022-07-11 19:55:12 -0700

    Update index.php

--- helloworld/index.php
+++ helloworld/index.php
@@ -1,5 +1,31 @@
 <?php
 
+$servername = "dbaas-db-93735-do-user-10071316-0.b.db.ondigitalocean.com";
+$port = 25060
+$username = "doadmin";
+$password = "mGWLPBiaB9gL5fHr";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+
 // Show all information, defaults to INFO_ALL
 phpinfo();
 
